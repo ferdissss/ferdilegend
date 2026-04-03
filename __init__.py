5469ec27fff05e2d7fcc58cd1d1cc8a18106b707
ferdilegend <133656395+ferdissss@users.noreply.github.com>
2026-04-03 23:10:04 +0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from .banner import show_banner
-from .core import FerdiSession
 from .scraper import TelegramScraper
 from .userbot import UserBotHelper
-from .utils import rate_limit, async_rate_limit
+from .utils import rate_limit
 
 __version__ = "0.3.0"
-__all__ = ["show_banner", "FerdiSession", "TelegramScraper", "UserBotHelper", "rate_limit"]
 
-# Otomatik banner
+__all__ = ["show_banner", "TelegramScraper", "UserBotHelper", "rate_limit"]
+
+# Paket her import edildiğinde otomatik güzel banner çıksın
 show_banner()
